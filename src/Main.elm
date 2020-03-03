833769c50ab8c64547e7181ead145529fc935924
Andy Park <atpark333@gmail.com>
2020-03-03 21:03:23 +0900

    Add button and compile

--- src/Main.elm
+++ src/Main.elm
@@ -1,17 +1,38 @@
 module Main exposing (..)
 
-import Html exposing (..)
-import Html.Attributes
+import Browser
+import Element exposing (..)
+import Element.Border as Border
+import Element.Input as Input
+import Html
 
+view model =
+    layout [] <|
+        column []
+            [ el [] <| text "Hi"
+            , text "Hallo"
+            , text <| String.fromInt model
+            , Input.button 
+                [Border.width 1
+                , padding 10
+                , Border.rounded 10
+                ]
+                { label = text "+1", onPress = Just Increase }
+            ]
 
-main : Html.Html msg
+update: a -> b -> number
+update msg model =
+    3
+
+main : Program () Int msg
 main =
-    div []
-        [ h1 [] [ text "Hi" ]
-        , text "Hallo"
-        , a [Html.Attributes.href "fsfd"] [text "link"]
-        , node "myelement" [] [text "my element"]
-        ]
+    Browser.sandbox
+    { init = 0
+    , view = view
+    , update = update
+    }
+
+-- f <| g <| d <| 3 = f ( g ( d 3 ) )
 
 sum : number -> number -> number
 sum a b =
